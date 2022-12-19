52e48e9da7bc7006481f724abc122f309fd3436a
Nejobac <nejobac@gmail.com>
2022-12-19 16:11:58 -0300

    se le ponen margenes y bordes

--- src/components/ConfigComponent.js
+++ src/components/ConfigComponent.js
@@ -1,6 +1,7 @@
+import { borderTop } from "@mui/system";
 import React, { useState, useEffect } from "react";
 import avatar from "../assets/img/avatar.svg";
-import"./Config.css";
+import "./Config.css";
 
 const initialUsuario = {
     nombreUsuario: "usuario",
@@ -45,143 +46,168 @@ const ConfigComponent = ({ usuarioEditado, setUsuarioEditado, userEdit }) => {
     */
     return (
         <div className="fondo">
-        <div class="card" className="config">
-            <div>
-                <div className="asdasda">
-                <div class="card-body flex">
-                    <h4>Foto de perfil</h4>
-                    <img
-                        src={avatar}
-                        class="img-fluid rounded-start"
-                        width={"10%"}
-                        />
-                </div>
+            <div class="card" className="config" style={{ border: '3px solid', borderColor: '#25a5d5', borderRadius: '3%' }}>
                 <div>
-                    <h5 class="card-title">Nombre de usuario</h5>
-                    <button
-                        type="button"
-                        class="btn btn-success"
-                        /*                 onClick={() => userEdit(state)}
-                        */              >
-                        Cambiar foto
-                        </button>
+                    <h4>Foto de perfil</h4>
+                    <div class="container"
+                        style={{
+                            display: 'flex',
+                            alignItems: 'start'
+                        }}>
+                        <img
+                            src={avatar}
+                            position={"relative"}
+                            width={"10%"}
+                        />
+                        <div >
+                            <h5 class="card-title">Nombre de usuario</h5>
+                            <br />
+                            <button type="button"style={{backgroundColor: '#25a5d5'}}
+                                class="btn"
+                        /*onClick={() => userEdit(state)}*/              
+                            >Cambiar foto
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            <form>
-                <div class="mb-3" id="NombreUsuario">
-                    <label for="Nombre" class="form-label">
-                        Nombre de Usuario
-                    </label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="NombreUsuario"
-                        placeholder="NombreUsuario"
-                        //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
-                        //lo siguiente
-                        name="nombreUsuario"/* 
-                                value={nombreUsuario}
+                <br /><br /><br />
+                <form className="formulario" >
+                    <div class="row">
+                        <div class="col" >
+                            <div class="mb-3 " id="NombreUsuario" >
+                                <label for="Nombre" class="form-label">
+                                    Nombre de Usuario
+                                </label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="NombreUsuario"
+                                    placeholder="NombreUsuario"
+                                    //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
+                                    //lo siguiente
+                                    /* 
+                                    name="nombreUsuario"
+                                    value={nombreUsuario}
+                                    onChange={handleInputChange} */
+                                ></input>
+                            </div>
+                            <div class="mb-3 " id="Nombre" >
+                                <label for="Nombre" class="form-label">
+                                    Nombre
+                                </label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="Nombre"
+                                    placeholder="Nombre"
+                                //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
+                                //lo siguiente
+                                /* 
+                                name="nombre"
+                                value={nombre}
                                 onChange={handleInputChange} */
-                    ></input>
-                </div>
-                <div class="mb-3" id="Nombre">
-                    <label for="Nombre" class="form-label">
-                        Nombre
-                    </label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="Nombre"
-                        placeholder="Nombre"
-                    //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
-                    //lo siguiente
-                    /* 
-                    name="nombre"
-                    value={nombre}
-                    onChange={handleInputChange} */
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label for="Apellido" class="form-label">
-                        Apellido
-                    </label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="apellido"
-                        placeholder="Apellido"
-                        name="apellido"/* 
+                                ></input>
+                            </div>
+                            <div class="mb-3">
+                                <label for="Apellido" class="form-label">
+                                    Apellido
+                                </label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="apellido"
+                                    placeholder="Apellido"
+                                    name="apellido"/* 
                                 value={apellido}
                                 onChange={handleInputChange} */
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label for="Correo" class="form-label">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        class="form-control"
-                        id="Correo"
-                        placeholder="email@ejemplo.com"
-                        name="correo"
-                    /*value={correo}
-                    onChange={handleInputChange} */
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label for="Password" class="form-label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        class="form-control"
-                        id="Password"
-                        name="password">
-                        {/* 
+                                ></input>
+
+                            </div>
+                            <button
+                                type="button"
+                                class="btn btn-success"
+                            // onClick={() => userEdit(state)}
+                            >Cancelar
+                            </button>
+                        </div>
+                        <div class="col">
+
+                            <div class="mb-3">
+                                <label for="Correo" class="form-label">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="Correo"
+                                    placeholder="email@ejemplo.com"
+                                    name="correo"
+                                /*value={correo}
+                                onChange={handleInputChange} */
+                                ></input>
+                            </div>
+                            <div class="mb-3">
+                                <label for="Password" class="form-label">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    id="Password"
+                                    name="password">
+                                    {/* 
                                 value={password}
                             onChange={handleInputChange}  */}
-                    </input>
-                </div>
-                <div class="mb-3" id="">
-                    <label for="fechaNacimiento" class="form-label">
-                        Fecha de Nacimiento
-                    </label>
-                    <input
-                        type="date"
-                        class="form-control"
-                        id="fechaNacimiento"
-                        placeholder="fechaNacimiento"
-                    //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
-                    //lo siguiente
-                    /* 
-                    name="FechaNacimiento"
-                    value={fechaNacimiento}
-                    onChange={handleInputChange} */
-                    ></input>
-                </div>
-                {/* 
+                                </input>
+                            </div>
+                            <div class="mb-3" id="">
+                                <label for="fechaNacimiento" class="form-label">
+                                    Fecha de Nacimiento
+                                </label>
+                                <input
+                                    type="date"
+                                    class="form-control"
+                                    id="fechaNacimiento"
+                                    placeholder="fechaNacimiento"
+                                //para que mi formulario sea capaz de crear un usuario debemos agregarle a los input
+                                //lo siguiente
+                                /* 
+                                name="FechaNacimiento"
+                                value={fechaNacimiento}
+                                onChange={handleInputChange} */
+                                ></input>
+                            </div>
+                            <button
+                                type="button"
+                                class="btn btn-success"
+                                /*                 onClick={() => userEdit(state)}
+                                */              >
+                                Continuar
+                            </button>
+                        </div>
+                    </div>
+                    {/* 
                         <button
                         type="button"
                         class="btn btn-success"
                         onClick={() => userEdit(state)}
                         >
                         Editar usuario
-                    </button> 
-                 {usuarioEditado !== null ? (
-                           <button
-                           type="button"
-                           class="btn btn-danger"
-                                onClick={() => setUsuarioEditado(null)}
-                                >
-                                Cancelar
-                                </button>
-                                ) : (
-                                    <></>
+                        </button> 
+                        {usuarioEditado !== null ? (
+                            <button
+                            type="button"
+                            class="btn btn-danger"
+                            onClick={() => setUsuarioEditado(null)}
+                            >
+                            Cancelar
+                            </button>
+                            ) : (
+                                <></>
+                                </div>
                                 )} */}
-            </form>
-        </div>
+                </form>
+            </div>
         </div>
     );
 };
